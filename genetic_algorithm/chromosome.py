c8be4d065ace93f4ab61fb3ea39e2ebc90c06ef3
Simon <simonmyhre1@gmail.com>
2022-11-07 12:00:27 +0100

    feat: create  chromosome

---
+++ genetic_algorithm/chromosome.py
@@ -0,0 +1,14 @@
+from random import random
+
+
+# Chromosome is a class that represents a single chromosome.
+# It has genes that are 0 or 1. When creates, this is random.
+class Chromosome:
+    def __init__(self, bit_string_length):
+        self.genes = []
+        for i in range(bit_string_length):
+            if random() < 0.5:
+                self.genes.append(0)
+            else:
+                self.genes.append(1)
+
