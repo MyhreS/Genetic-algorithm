104b67484762d5ad0596de3bd3c7084eb746322d
Simon <simonmyhre1@gmail.com>
2022-11-07 12:19:39 +0100

    docs: describe chromosome

--- genetic_algorithm/chromosome.py
+++ genetic_algorithm/chromosome.py
@@ -1,8 +1,12 @@
 from random import random
 
-
-# Chromosome is a class that represents a single chromosome.
-# It has genes that are 0 or 1. When creates, this is random.
+"""
+Chromosome is a class that represents a single chromosome.
+It has genes that are 0 or 1. When creates, this is random.
+The amount of genes is determined by the user through the bit_string_length parameter in the constructor.
+"""
+#
+#
 class Chromosome:
     def __init__(self, bit_string_length):
         self.genes = []
@@ -12,11 +16,23 @@ class Chromosome:
             else:
                 self.genes.append(1)
 
+
+    """
+    This method is used to change a gene in the chromosome.
+    It takes the index of the gene to change and the new value of the gene.
+    It returns the old value of the gene.
+    """
     def change_gene(self, index, value):
         old_gene = self.genes[index]
         self.genes[index] = value
         return old_gene
 
+
+    """
+    THis method is used to mutate a chromosome.
+    It takes the mutation rate as a parameter.
+    It returns the chromosome with the mutated genes.
+    """
     def mutate_chromosome(self, mutation_rate):
         for i in range(len(self.genes)):
             if random() < mutation_rate:
@@ -25,6 +41,11 @@ class Chromosome:
                 else:
                     self.genes[i] = 0
 
+
+    """
+    This method is used to print the chromosome.
+    It returns a string of the chromosome.
+    """
     def __str__(self):
         return "{}".format(self.genes)
 
