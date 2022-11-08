fa43feb4d57a98468b74b2223d3949db89347826
Simon <simonmyhre1@gmail.com>
2022-11-08 18:02:11 +0100

    feat: stop evolution when top is reached

--- genetic_algorithm/genetic_algorithm.py
+++ genetic_algorithm/genetic_algorithm.py
@@ -1,4 +1,3 @@
-
 import population as pop
 
 """
@@ -6,6 +5,7 @@ A class that is used to represent the generic algorithm.
 It craetes a population and evolves it by applying the genetic operators.
 It performes elitism, and reproduction by selection, crossover and mutation.
 """
+
 class GeneticAlgorithm:
 
     def __init__(self, generations, population_size, bit_string_length, elite_rate, crossover_rate, mutation_rate):
@@ -26,6 +26,12 @@ class GeneticAlgorithm:
 
         for i in range(self.generations):
             population.evolve(self.elite_rate, self.crossover_rate, self.mutation_rate)
+
+            if population.log_best_fitness[-1] == population.population_size:
+                break
+        # Print the best results log.
+        for fitness in population.log_best_fitness:
+            print(fitness)
 
 
 
